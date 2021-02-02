c20bbf6362abde590cb4b77d7be21b59e2307ca3
Fernando de Sá <mluizsa@gmail.com>
2021-02-02 18:15:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, ress) => {
     ress.status(200).send({ message: 'Olá mundo, estou funcionando' });
 })
 
+app.get('/ola', (req, ress) => {
+    ress.status(200).send({ message: 'OI eu sou a rota Olá' });
+})
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
